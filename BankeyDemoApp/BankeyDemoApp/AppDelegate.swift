aaaaa05f5302c0b5841bb0e0de7f4dddfa111dce
Mahesh Kulkarni <maheshk@scmcpvt.com>
2022-01-15 12:11:20 +0530

    Add first onboarding screen

--- BankeyDemoApp/BankeyDemoApp/AppDelegate.swift
+++ BankeyDemoApp/BankeyDemoApp/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = OnboardingContainerViewController()
+        window?.rootViewController = OnboardingViewController()
+       // OnboardingContainerViewController
         return true
     }
 }
